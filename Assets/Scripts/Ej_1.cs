5768f9be174f4544107d962eec75cf8cdc4681bf
AgustinRossi123 <101335570+AgustinRossi123@users.noreply.github.com>
2022-04-11 11:36:10 -0300

    Ej_01

---
+++ Assets/Scripts/Ej_1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float monto;
+    public string moneda;
+    void Start()
+    {
+        float calculo;
+
+        if (monto < 1000){
+            Debug.Log("El monto mínimo es 1000");
+        }
+        else if (monto >= 1000)
+        {
+
+        
+        if (moneda == "D")
+        {
+                calculo = monto / 112;
+                Debug.Log("Su monto de " + monto + " pesos equivale a "+calculo+" en dolares");
+        }
+        else if (moneda == "R")
+        {
+                calculo = monto / 23;
+                Debug.Log("Su monto de " + monto + " pesos equivale a " + calculo + " en reales");
+            }
+        else if (moneda == "E")
+        {
+                calculo = monto / 122;
+                Debug.Log("Su monto de " + monto + " pesos equivale a " + calculo + " en euros");
+            }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+        }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
